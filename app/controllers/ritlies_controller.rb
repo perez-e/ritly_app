27ff7b66adb9271a40ba94c5232cbdc2994208f6
Eric Perez <ericricardoperez@gmail.com>
2014-01-25 16:39:45 -0800

    rails g controller ritlies index

---
+++ app/controllers/ritlies_controller.rb
@@ -0,0 +1,37 @@
+class RitliesController < ApplicationController
+
+  def index
+  end
+
+  def create
+  	generate_str = SecureRandom.urlsafe_base64(10)
+  	r = Ritly.new
+  	r.random_string = generate_str
+  	r.visits = 0
+  	link = params[:ritly][:link]
+  	link.slice!("http://")
+  	full_url = ["http://", link].join
+  	r.link = full_url
+  	r.save
+
+  	redirect_to action: :show, random_str: generate_str
+  end
+
+  def show
+  	@ritly = Ritly.where(random_string: params[:random_str]).first
+  end
+
+  def redirect
+  	ritly = Ritly.where(random_string: params[:random_str]).first
+  	ritly.visits += 1
+  	ritly.save
+
+  	redirect_to ritly.link
+  end
+
+  def all
+  	@links = Ritly.all
+  	
+  end
+
+end
